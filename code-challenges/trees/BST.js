76d8cdf6a38210d6a3f779c6244b031ecd009180
Jonathan DiQuattro <jondiquattro@hotmail.com>
2019-01-16 06:29:07 -0800

    added read me

---
+++ code-challenges/trees/BST.js
@@ -0,0 +1,84 @@
+'use strict';
+
+
+
+class Node {
+    constructor(value){
+        this.value =value;
+        this.right = null;
+        this.left = null;
+        
+    }
+}
+
+
+class BinaryTreeClass{
+    constructor(){
+        this.root = null;//sets root to null
+    }
+
+    preOrder(){
+    let nodes = [];
+
+    let _walk = (node) =>{
+
+    nodes.push(node.value);
+    
+    if(node.left){_walk(node.left)}  //if there is a node.left walk left
+    if(node.right){_walk(node.right)}//if there is a node.right walk right
+    };
+
+    _walk(this.root);
+    return nodes;
+    }
+
+    postOrder(){
+        let nodes = [];
+        
+        let _walk = (node)=>{
+            if(node.left){_walk(node.left)};
+            if(node.right){_walk(node.right)};
+            nodes.push(node.value);
+
+        };
+
+
+        _walk(this.root);//calls the walk function so that it runs
+        return nodes;  //returns node array
+    }
+
+    inOrder(){
+        let nodes = [];
+    
+        let _walk = (node) =>{
+    
+        
+        if(node.left){_walk(node.left)}  //if there is a node.left walk left
+        nodes.push(node.value);
+
+        if(node.right){_walk(node.right)}//if there is a node.right walk right
+        };
+    
+        _walk(this.root);
+        return nodes;
+        }
+
+    breadthFirst(tree){
+
+        let queue = [];
+        queue.push(tree.root);
+
+        while(queue.length){
+            console.log(queue[queue.length]);
+            let current =queue.shift();
+            if(current.left){
+            queue.push(current.left);
+
+            }
+            if(current.right){
+                queue.push(current.right);
+
+            }
+        }
+    }
+}
