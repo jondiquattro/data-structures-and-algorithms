4abe39382dc723ba2b6aa97b737d9a4e6700b201
Jonathan DiQuattro <jondiquattro@hotmail.com>
2019-01-27 18:56:04 -0800

    bst code

--- code-challenges/trees/BST.js
+++ code-challenges/trees/BST.js
@@ -1,72 +1,118 @@
 'use strict';
 
 class Node {
-    constructor(value){
-        this.value =value;
+    constructor(value) {
+        this.value = value;
         this.right = null;
         this.left = null;
-        
+
     }
 }
 //////////////////////////
 //takes in a tree dummy stop over thinking it!
 
-class BinaryTree{
-    constructor(){
+class BinaryTree {
+    constructor() {
         this.root = null;//sets root to null
     }
 
-    preOrder(){
-    let nodes = [];
-
-    //takes a node
-    let _walk = (node) =>{
-
-    nodes.push(node.value);
-    
-    if(node.left){_walk(node.left)}  //if there is a node.left walk left
-    if(node.right){_walk(node.right)}//if there is a node.right walk right
-    };
-    //checks for null tree
-    if(this.root !== null){
-        _walk(this.root);
-    }
-    return nodes;
-    }
-
-    postOrder(){
+    preOrder() {
         let nodes = [];
-        
-        let _walk = (node)=>{
-            if(node.left){_walk(node.left)};
-            if(node.right){_walk(node.right)};
+
+        //takes a node
+        let _walk = (node) => {
+
+            nodes.push(node.value);
+
+            if (node.left) { _walk(node.left) }  //if there is a node.left walk left
+            if (node.right) { _walk(node.right) }//if there is a node.right walk right
+        };
+        //checks for null tree
+        if (this.root !== null) {
+            _walk(this.root);
+        }
+        return nodes;
+    }
+
+    postOrder() {
+        let nodes = [];
+
+        let _walk = (node) => {
+            if (node.left) { _walk(node.left) };
+            if (node.right) { _walk(node.right) };
             nodes.push(node.value);
 
         };
 
 
-        if(this.root !== null){
+        if (this.root !== null) {
             _walk(this.root);
         }
         return nodes;  //returns node array
     }
 
-    inOrder(){
+    inOrder() {
         let nodes = [];
-    
-        let _walk = (node) =>{
-    
-        
-        if(node.left){_walk(node.left)}  //if there is a node.left walk left
-        nodes.push(node.value);
 
-        if(node.right){_walk(node.right)}//if there is a node.right walk right
+        let _walk = (node) => {
+
+
+            if (node.left) { _walk(node.left) }  //if there is a node.left walk left
+            nodes.push(node.value);
+
+            if (node.right) { _walk(node.right) }//if there is a node.right walk right
         };
-        if(this.root !== null){
+        if (this.root !== null) {
             _walk(this.root);
         }
         return nodes;
+    }
+
+    breadthFirst() {
+        let order = [];
+        let queue = [];
+
+        queue.push(this.root);
+
+        while (queue.length) {
+
+            let current = queue.shift();
+            if(current === null){
+                return null;
+            }
+            console.log(current.value);
+
+            order.push(current.value);
+
+            if (current.left) {queue.push(current.left);}
+            if (current.right) {queue.push(current.right);}
         }
+        return order;
+    }
+
+    findMaximumValue(){
+    
+        if(!this.root){return null}
+
+        let max = this.root.value;
+
+        let queue = [];
+
+        queue.push(this.root);
+
+        while(queue.length){
+
+            let node = queue.shift();
+
+            if (max<node.value){
+                max = node.value
+            }
+
+            if(node.left){queue.push(node.left);
+            if(node.right){queue.push(node.right)}}
+        }
+        return max;
+    }
 }
 
 
@@ -75,31 +121,31 @@ class BinaryTree{
 
 
 //add should take a node and insert it into the tree
-class BinarySearchTree{
-    constructor(value){
-    this.root = null;
-    this.value = null;
+class BinarySearchTree {
+    constructor(value) {
+        this.root = null;
+        this.value = null;
     }
 
-    add(value){
+    add(value) {
         //create a node with the value
         this.value = value;
         // find the next logical place for the node
-        let _walk = (node) =>{
-            
+        let _walk = (node) => {
 
-            if(this.value < node.value){
 
-                if(node.left){_walk(node.left)} 
+            if (this.value < node.value) {
 
-                else if (!node.left){
+                if (node.left) { _walk(node.left) }
+
+                else if (!node.left) {
                     node.left = new Node(this.value);
                 }
             }
-            if(this.value > node.value){
-                if(node.right){_walk(node.right)}
+            if (this.value > node.value) {
+                if (node.right) { _walk(node.right) }
                 //after recursion is over
-                else if (!node.right){
+                else if (!node.right) {
                     node.right = new Node(this.value);
                 }
 
@@ -107,19 +153,19 @@ class BinarySearchTree{
 
         };
         //if tree is not empty
-        if (this.root){
+        if (this.root) {
             _walk(this.root);
         }
-        else{
+        else {
             this.root = new Node(value);
         }
     }
-print(){
-    console.log(this.root);
+    print() {
+        console.log(this.root);
 
+    }
 }
-}
 
 
 
-module.exports = {BinarySearchTree,BinaryTree, Node};
+module.exports = { BinarySearchTree, BinaryTree, Node };
--- code-challenges/trees/__test__/BST.test.js
+++ code-challenges/trees/__test__/BST.test.js
@@ -2,7 +2,7 @@
 const Tree = require('../BST');
 
 describe( 'Binary Trees should contain pre, in, post order treversal methods', ()=>{
-    describe('Binary tree preorder method should be able to do the following:', ()=>{
+    xdescribe('Binary tree preorder method should be able to do the following:', ()=>{
         it('should be able to take in a tree and traverse it in prefix order', ()=>{
     
             let tree = new Tree.BinaryTree;
@@ -46,7 +46,7 @@ describe( 'Binary Trees should contain pre, in, post order treversal methods', (
         })
     })
 
-    describe('Binary tree Inorder method should be able to do the following:', ()=>{
+    xdescribe('Binary tree Inorder method should be able to do the following:', ()=>{
         it('should be able to take in a tree and traverse it in an in line order', ()=>{
     
             let tree = new Tree.BinaryTree;
@@ -88,9 +88,10 @@ describe( 'Binary Trees should contain pre, in, post order treversal methods', (
     
             expect(tree.preOrder(a)).toEqual(['a']);
         })
+
     })
 
-    describe('Binary tree postorder method should be able to do the following:', ()=>{
+    xdescribe('Binary tree postorder method should be able to do the following:', ()=>{
         it('should be able to take in a tree and traverse it in a post order', ()=>{
     
             let tree = new Tree.BinaryTree;
@@ -134,11 +135,90 @@ describe( 'Binary Trees should contain pre, in, post order treversal methods', (
         })
     })
 
+    describe('Breadth first should do the following:', ()=>{
+        it('should be able to traverse the tree by breadth instead of depth', ()=>{
+            let tree = new Tree.BinaryTree;
 
+            let a = new Tree.Node('a');
+            let b = new Tree.Node('b');
+            let c = new Tree.Node('c');
+            let d = new Tree.Node('d');
+            let e = new Tree.Node('e');
+            let f = new Tree.Node('f');
+    
+            a.left = b;
+            a.right = c;
+            b.right = d;
+            b.left = e;
+            c.right = f;
+            
+            tree.root = a;
+            expect(tree.breadthFirst(a)).toEqual(['a', 'b','c','e','d','f']);
+        })
+        it('should traverse a tree of one', ()=>{
+            let tree = new Tree.BinaryTree();
+            let a = new Tree.Node('a');
+            tree.root = a;
+            expect(tree.breadthFirst()).toEqual(['a']);
+        })
+        it('should traverse an empty tree', ()=>{
+            let tree = new Tree.BinaryTree();
+            // let a = new Tree.Node('a');
+            // tree.root = a;
+            expect(tree.breadthFirst()).toEqual(null);
+        })
+    })
+    describe('findMaxValue should return the largest value in a tree', ()=>{
+
+        it('should find the max value', ()=>{
+            let tree = new Tree.BinaryTree;
+
+            let a = new Tree.Node(1);
+            let b = new Tree.Node(2);
+            let c = new Tree.Node(3);
+            let d = new Tree.Node(6);
+            let e = new Tree.Node(5);
+            let f = new Tree.Node(4);
+    
+            a.left = b;
+            a.right = c;
+            b.right = d;
+            b.left = e;
+            c.right = f;
+            
+            tree.root = a;
+            expect(tree.findMaximumValue()).toEqual(6);
+        })
+        it('should ignore anything that isnt a number', ()=>{
+            let tree = new Tree.BinaryTree;
+
+            let a = new Tree.Node(1);
+            let b = new Tree.Node('a');
+            let c = new Tree.Node(3);
+            let d = new Tree.Node(6);
+            let e = new Tree.Node('b');
+            let f = new Tree.Node(4);
+    
+            a.left = b;
+            a.right = c;
+            b.right = d;
+            b.left = e;
+            c.right = f;
+            
+            tree.root = a;
+            expect(tree.findMaximumValue()).toEqual(6);
+        })
+
+        it('should return null on an empty tree', ()=>{
+            let tree = new Tree.BinaryTree();
+            expect(tree.findMaximumValue()).toEqual(null);
+
+        })
+    })
 })//end Binary Tree Class
 
 
-describe( 'BinarySearchTree Class should contain the add method that does the following', ()=>{
+xdescribe( 'BinarySearchTree Class should contain the add method that does the following', ()=>{
     describe('the add mathod should use a recursive binary search and add a node to the next logical place in the tree', ()=>{
 
         it('should find the next logical place in the tree', ()=>{
